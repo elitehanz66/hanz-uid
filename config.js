1a467a3e5567a753881b317ebbc2e0aeca047a56
elitehanz66 <elitehanz66@gmail.com>
2026-02-14 16:51:08 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkikbtAZR22Tn57JW7dxVhHITpqiKYKR0",
+  authDomain: "hanz-cihuy.firebaseapp.com",
+  databaseURL: "https://hanz-cihuy-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hanz-cihuy",
+  storageBucket: "hanz-cihuy.firebasestorage.app",
+  messagingSenderId: "160775784054",
+  appId: "1:160775784054:web:4ea975575336592f904157",
+  measurementId: "G-LJXGP7QQGX"
+};
+window.firebaseConfig = firebaseConfig;
